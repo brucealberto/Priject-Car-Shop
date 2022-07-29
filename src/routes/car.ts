1cbb10ca294fae7b311354dd3b492460c9ad5d1f
brucealberto <brucealberto2@gmail.com>
2022-07-29 19:29:03 -0300

    feat: carController

---
+++ src/routes/car.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import Car from '../models/Cars';
+import CarService from '../services/CarService';
+
+const route = Router();
+
+const car = new Car();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+route.post('/cars', (req, res) => carController.create(req, res));
+
+export default route;
