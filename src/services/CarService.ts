495fd84e3c35c926fc333c23e72aec52f27c0928
brucealberto <brucealberto2@gmail.com>
2022-07-29 19:10:43 -0300

    feat: CarService

---
+++ src/services/CarService.ts
@@ -0,0 +1,16 @@
+import { CarZodSchema, ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { IService } from '../interfaces/IService';
+
+export default class CarService implements IService<ICar> {
+  private _car: IModel<ICar>;
+  constructor(model: IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj: ICar): Promise<ICar> {
+    const parsed = CarZodSchema.safeParse(obj);
+    if (!parsed.success) throw parsed.error;
+    return this._car.create(obj);
+  }
+}
