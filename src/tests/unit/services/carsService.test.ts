6a9945a9fba25c62e63962624e6ba842da28f014
brucealberto <brucealberto2@gmail.com>
2022-08-01 10:46:58 -0300

    feat: complete service tests

--- src/tests/unit/services/carsService.test.ts
+++ src/tests/unit/services/carsService.test.ts
@@ -5,6 +5,7 @@ import CarService from '../../../services/CarService';
 import { Model } from 'mongoose';
 import { carsMock, carsMockWithId } from '../../mocks/carsMock';
 import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalogs';
 const { expect } = chai;
 
 describe('Cars Service', () => {
@@ -12,7 +13,10 @@ describe('Cars Service', () => {
   const carsService = new CarService(carsModel);
 
   before(async () => {
-    sinon.stub(Model, 'create').resolves(carsMockWithId);
+    sinon.stub(carsModel, 'create').resolves(carsMockWithId);
+    sinon.stub(carsModel, 'readOne').onCall(0).resolves(carsMockWithId)
+    .onCall(1).resolves(null).onCall(2).resolves(carsMockWithId)
+    sinon.stub(carsModel, 'update').resolves(carsMockWithId);
   });
 
   after(() => {
@@ -20,8 +24,8 @@ describe('Cars Service', () => {
   });
 
   it('Created Car', async () => {
-    const car = await carsService.create(carsMock);
-    expect(car).to.be.equal(carsMockWithId);
+    const carCreated = await carsService.create(carsMock);
+    expect(carCreated).to.be.equal(carsMockWithId);
   });
 
   it('Car Not Created', async () => {
@@ -31,4 +35,24 @@ describe('Cars Service', () => {
       expect(error).to.be.instanceOf(ZodError);
     }
   });
+
+  it('Found Car Id', async () => {
+    const carFoundId = await carsService.readOne(carsMockWithId._id)
+    expect(carFoundId).to.be.deep.equal(carsMockWithId);
+  });
+
+  it('Not Found Car Id', async () => {
+  try {
+      await carsService.readOne(carsMockWithId._id)
+    } catch (error:any) {
+    expect(error.message).to.be.deep.equal(ErrorTypes.ObjectNotFound);
+  }
+  });
+
+  it('Updated Car', async () => {
+    const carUpdated = await carsService.update('4edd40c86762e0fb12000003', carsMock)
+    expect(carUpdated).to.be.deep.equal(carsMockWithId);
+  });
+
+
 });
