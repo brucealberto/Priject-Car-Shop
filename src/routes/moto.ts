8dc1fffea1ac63f693561c01e3e7962c8b2dd008
brucealberto <brucealberto2@gmail.com>
2022-08-01 18:04:37 -0300

    feat: delete motorcycle route

--- src/routes/moto.ts
+++ src/routes/moto.ts
@@ -9,11 +9,12 @@ const motorcycle = new Motorcycle();
 const motorcycleService = new MotorcycleService(motorcycle);
 const motorcycleController = new MotorcycleController(motorcycleService);
 
+const motorcycleId = '/motorcycles/:id';
+
 route.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
 route.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
-route.get('/motorcycles/:id', (req, res) =>
-  motorcycleController.readOne(req, res));
-route.put('/motorcycles/:id', (req, res) =>
-  motorcycleController.update(req, res));
+route.get(motorcycleId, (req, res) => motorcycleController.readOne(req, res));
+route.put(motorcycleId, (req, res) => motorcycleController.update(req, res));
+route.delete(motorcycleId, (req, res) => motorcycleController.delete(req, res));
 
 export default route;
